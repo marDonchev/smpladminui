aadc1e37e096c11cf0921e84ba79470b32bfd08e
Martin D <mdonchev@msn.com>
2020-12-28 12:41:24 +0200

    Fixes

--- src/Toggle/ToggleComponent.jsx
+++ src/Toggle/ToggleComponent.jsx
@@ -25,7 +25,7 @@ class ToggleComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: false
+      value: this.props.value?this.props.value:false
     };
   }
 
